2af43622bb3f7b378fd71ced171c578b757923bd
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-21 10:36:03 +0900

    seedsで追加していたデータを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,8 +6,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-item = Item.create(
-  name: 'test_name',
-  price: 1000
-)
-item.thumbnail_image.attach(io: File.open(Rails.root.join('app/assets/images/book.png')), filename: 'book.png')
