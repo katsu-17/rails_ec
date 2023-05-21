689dac32a276362b7dcf28d84eeda4f7fa55e542
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-21 10:25:50 +0900

    validationの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { only_integer: true }
+
   has_one_attached :thumbnail_image
 end
