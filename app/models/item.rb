b442570c4ef297378ac14ebb87a0266ede13737b
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-21 11:53:34 +0900

    画像が登録されていない時の処理を追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,5 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { only_integer: true }
 
-  has_one_attached :thumbnail_image
+  has_one_attached :image
 end
