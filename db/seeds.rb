7ab28c202d9c1c88242798876156e83e1fbe7182
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-24 23:25:47 +0900

    seedsの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-item = Item.create(
+item = Item.new(
   name: 'macbook',
   price: 3000
 )
-item.image.attach(io: File.open(Rails.root.join('public/assets/macbook-16daa50489c25572a85aca8e1e7d4292caf2cb33a3ac689e8e0c3dd0a82cd655.jpg'), filename: 'macbook.jpg'))
+item.image.attach(
+  io: File.open(Rails.root.join('db/seeds/macbook.jpg')),
+  filename: 'macbook.jpg',
+  content_type: 'image/jpeg'
+)
+item.save!
