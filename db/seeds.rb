6b7771f4cf636199927fee89098324aad5bf9224
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-26 00:11:25 +0900

    seeds.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 Item.create(
   name: 'macbook',
   price: 3000,
-  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('db/seeds/macbook.jpg')), filename: 'a')
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open('db/seeds/macbook.jpg'), filename: 'mac.jpg')
 )
