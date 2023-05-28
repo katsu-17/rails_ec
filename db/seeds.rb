ce1409e05982991a5cefd096f38a081a5706581b
katsu-17 <tsutsui.katsushi@gmail.com>
2023-05-28 15:58:33 +0900

    別の画像に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Item.create(
-  name: 'macbook',
-  price: 3000,
-  image: ActiveStorage::Blob.create_and_upload!(io: File.open('db/seeds/macbook.jpg'), filename: 'mac.jpg')
+  name: 'cup',
+  price: 1200,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open('db/seeds/cup.jpg'), filename: 'cup.jpg')
 )
