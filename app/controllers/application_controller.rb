d4e1bef9a75616f80c9de3cd70102b7918352aef
katsu-17 <tsutsui.katsushi@gmail.com>
2023-06-14 23:54:14 +0900

    メソッド名の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@
 
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
-  helper_method :current_cart
+  helper_method :current_cart_items
 
   private
 
-  def current_cart
+  def current_cart_items
     @cart_items = CartItem.includes(:item).where(cart_id: session[:cart_id])
   end
 end
