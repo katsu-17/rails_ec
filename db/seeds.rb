3430b93c085cce6e13598dbc223e71bb563e56e8
katsu-17 <tsutsui.katsushi@gmail.com>
2023-06-16 00:16:18 +0900

    seedを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,33 @@
 
 item = Item.new(
   name: 'cup',
-  price: 1000
+  price: 400
 )
 item.image.attach(
   io: File.open(Rails.root.join('db/seeds/cup.jpg')),
   filename: 'cup.jpg',
+  content_type: 'image/jpeg'
+)
+item.save
+
+item = Item.new(
+  name: 'book',
+  price: 1000
+)
+item.image.attach(
+  io: File.open(Rails.root.join('db/seeds/book.jpg')),
+  filename: 'book.jpg',
+  content_type: 'image/jpeg'
+)
+item.save
+
+item = Item.new(
+  name: 'Macbook',
+  price: 10000
+)
+item.image.attach(
+  io: File.open(Rails.root.join('db/seeds/macbook.jpg')),
+  filename: 'macbook.jpg',
   content_type: 'image/jpeg'
 )
 item.save
