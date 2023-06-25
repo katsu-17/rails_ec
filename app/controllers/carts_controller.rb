5e89a4af8b7778a19404a1f1be40c98e8c9988f4
katsu-17 <tsutsui.katsushi@gmail.com>
2023-06-26 00:05:51 +0900

    saveがfalseの時の処理を追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,7 +8,12 @@ class CartsController < ApplicationController
     if PromotionCode.find_by(code: params[:code])&.unused?
       cart = Cart.find(session[:cart_id])
       cart.promotion_code = params[:code]
-      cart.save!
+      if cart.save
+        redirect_to carts_path
+      else
+        flash.now[:danger] = 'エラーが発生しました。もう一度入力してください。'
+        render 'index', status: :unprocessable_entity
+      end
     else
       flash.now[:danger] = '入力されたプロモーションコードは無効です'
       render 'index', status: :unprocessable_entity
